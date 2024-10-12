c7b7869805598239fa9cfcf3adf83c0ca8827108
Piyush <piyushsuthar6300@gmail.com>
2024-10-12 17:18:31 +0530

    Reverse an Array

---
+++ ReverseElementsOfArray.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class ReverseElementsOfArray{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter Number Of Elements : ");
+        int n = input.nextInt();
+
+        int [] a = new int[n];
+        for(int i=0 ; i<n ; i++){
+            System.out.print("Enter element " + (i+1) + " : ");
+            a[i] = input.nextInt();
+        }
+
+        int start = 0;
+        int end = n-1;
+        while(start<end){
+            int temp = a[start];
+            a[start] = a[end];
+            a[end] = temp;
+            start++;
+            end--;
+        }
+
+        System.out.println("Reversed Array : ");
+
+        for(int i=0 ; i<n ; i++){
+            System.out.print(a[i] + " ");
+        }
+
+        input.close();
+    }
+}
