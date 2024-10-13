927c139fa2dd8d9a6afefb138cb9500e6d0db373
Piyush <piyushsuthar6300@gmail.com>
2024-10-13 23:22:24 +0530

    ArrayList Initialization and Printing

---
+++ ArrayListInitialization.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ArrayListInitialization{
+    public static void main(String[] args){
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(10,20,30));
+
+        list.add(10);
+        list.add(4,99);
+        
+        System.out.println(list);
+
+    }
+}
