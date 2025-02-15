d82deaa286e6dd9a0aff3f26bffc5cd06d5db943
Piyush <piyushsuthar6300@gmail.com>
2025-02-15 12:13:42 +0530

    Sorted Divisor using List with O(√n) TC

--- Basics/PrintDivisorTimeComplexity.java
+++ Basics/PrintDivisorTimeComplexity.java
@@ -1,19 +1,32 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
+import java.util.Collections;
 public class PrintDivisorTimeComplexity {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Number : ");
         int n = sc.nextInt();
 
+        ArrayList <Integer> res = new ArrayList<>();
+
         for(int i=1 ; i<=Math.sqrt(n) ; i++){
             if(n%i==0){
-                System.out.print(i + " ");
+                res.add(i);
                 if(n/i!=i){
-                    System.out.print(n/i + " ");
+                    res.add(n/i);
                 }
             }
-            
         }
+        
+        //before sorting
+        System.out.println("Before sorting : ");
+        System.out.println(res);
+        
+        //after sorting
+        System.out.println("After sorting : ");
+        Collections.sort(res);
+        System.out.println(res);
+    
     }    
 }
