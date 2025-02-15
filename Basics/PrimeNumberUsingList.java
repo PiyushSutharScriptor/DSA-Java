b5c864eb6287785bd47e6793de2a7c6539d3b038
Piyush <piyushsuthar6300@gmail.com>
2025-02-15 12:48:59 +0530

    Prime Number using ArrayList O(√n)

---
+++ Basics/PrimeNumberUsingList.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class PrimeNumberUsingList {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int n = sc.nextInt();
+
+        ArrayList <Integer> li = new ArrayList<>();
+
+        for(int i=1 ; i<=n ; i++){
+            if(n%i==0){
+                li.add(i);
+            }
+        }
+
+        if(li.size()==2){
+            System.out.println("Prime Number");
+        }
+        else{
+            System.out.println("Not a prime number");
+        }
+
+        sc.close();
+    }
+}
