8c5fb06047e54fe19052fefe131f3879956564fe
Piyush <piyushsuthar6300@gmail.com>
2025-02-17 06:29:03 +0530

    Printing n to 1 uisng backtrack

---
+++ Recursion/PrintNto1UsingBacktrack.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Number{
+    public void print(int n, int count){
+        if(count>n){
+            return;
+        }
+        print(n, count+1);
+        System.out.println(count);
+    }
+}
+public class PrintNto1UsingBacktrack {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter N value : ");
+        int n = sc.nextInt();
+
+        Number n1 = new Number();
+        n1.print(n, 1);
+
+    }
+}
