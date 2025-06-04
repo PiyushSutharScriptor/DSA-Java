8b03d3647af7bcb25831d82ec8134f5a99e5c2d2
Piyush <piyushsuthar6300@gmail.com>
2025-06-04 18:01:26 +0530

    Find the element by row and col (better)

---
+++ Arrays/FindElementByPascalTriangleBetter.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class FindElementByPascalTriangleBetter
+{
+    public static int factorial(int n , int r){
+        int result = 1;
+        for(int i=0;i<r;i++){
+            result = result*(n-i);
+            result = result/(i+1);
+        }
+        return result;
+    }
+    
+    
+	public static void main(String[] args) {
+	    
+	    int row = 5;
+	    int col = 3;
+	    
+	    
+	    int n = row-1;
+	    int r = col-1;
+	    
+	    int sol = factorial(n,r);
+	    
+	    System.out.println("The element : " + sol);
+	    
+	}
+}
