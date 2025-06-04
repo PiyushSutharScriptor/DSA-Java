454958d55cb60aedc6698f75a4fd2b8a3938ea74
Piyush <piyushsuthar6300@gmail.com>
2025-06-04 20:35:14 +0530

    Print pascal triangle row (optimized)

---
+++ Arrays/PrintPascalTriangleRowOptimized.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class PrintPascalTriangleRowOptimized
+{
+	public static void main(String[] args) {
+	    
+	    int row = 6;
+	    
+	    int ans = 1; 
+	    System.out.print(ans + " ");
+	    
+	    for(int i=1 ; i<row ; i++){
+	        ans = ans * (row-i);
+	        ans = ans/i;
+	        System.out.print(ans+" ");
+	    }
+	    
+	}
+}
