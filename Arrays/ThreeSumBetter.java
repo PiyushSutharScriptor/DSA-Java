32b48dd27d07b156288e95d5867edde6cb161105
Piyush <piyushsuthar6300@gmail.com>
2025-06-06 22:45:28 +0530

    Update ThreeSumBetter.java

--- Arrays/ThreeSumBetter.java
+++ Arrays/ThreeSumBetter.java
@@ -2,9 +2,12 @@ import java.util.*;
 
 public class ThreeSumBetter {
 	public static void main(String[] args) {
+
 	    int arr[] = {-1,0,1,2,-1,-4};
 	    int n = arr.length;
+
 	    Set<List<Integer>> set = new HashSet<>();
+        
 	    Arrays.sort(arr);
 	    
 	    for(int i=0;i<n;i++){
