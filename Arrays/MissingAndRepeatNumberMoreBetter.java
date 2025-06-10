ce17f9c6965e33870c33ee6daec6b64606f36e7a
Piyush <piyushsuthar6300@gmail.com>
2025-06-10 18:20:54 +0530

    Missing and repeat number more better

---
+++ Arrays/MissingAndRepeatNumberMoreBetter.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class MissingAndRepeatNumberMoreBetter
+{
+	public static void main(String[] args) {
+	    
+	    int arr[] = {4,3,6,2,1,1};
+	    
+	    int n = arr.length;
+	    int repeat = -1;
+	    int missing = -1;
+	    
+	    
+	    for(int i=1 ; i<=n ; i++){
+	        int count = 0;
+	                        
+	        for(int j=0 ; j<n ; j++){
+	            if(arr[j]==i){
+	                count++;
+	            }
+	            
+	        }
+	        
+	       if(count==0) missing = i;
+	       if(count>1) repeat = i;
+	       
+	       if(repeat!=-1 && missing!=-1){
+	           break;
+	       }
+	    }
+	    
+	    System.out.println("Missing Number : " + missing);
+	    System.out.println("Repeating Number : " + repeat);
+	    
+	}
+}
