b3b37f3475b9499f433c89d361f5d5a94682040a
Piyush <piyushsuthar6300@gmail.com>
2025-06-11 22:29:54 +0530

    Update MissingAndRepeatNumberBrute.java

--- Arrays/MissingAndRepeatNumberBrute.java
+++ Arrays/MissingAndRepeatNumberBrute.java
@@ -17,6 +17,7 @@ public class MissingAndRepeatNumberBrute
 	        }
 	    }
 	    
+		//missing number
 	    for(int i=1 ; i<=n ; i++){
 	        int count = 0;
 	        for(int j=0 ; j<n ; j++){
