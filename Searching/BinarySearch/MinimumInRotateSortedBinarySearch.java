08e9cbd82df69b19063e73d34ac7ef8ff003e373
Piyush <piyushsuthar6300@gmail.com>
2025-06-15 14:19:28 +0530

    Minimum in rotated sorted array (binary search)

---
+++ Searching/BinarySearch/MinimumInRotateSortedBinarySearch.java
@@ -0,0 +1,32 @@
+
+public class MinimumInRotateSortedBinarySearch
+{
+	public static void main(String[] args) {
+	    int arr[] = {23,24,25,11,13,15,17};
+	    int n = arr.length;
+	    
+	    int low = 0;
+	    int high = n-1;
+	    int min = Integer.MAX_VALUE;
+	    
+	    while(low<high){
+	        int mid = (low+high)/2;
+	        
+	        if(arr[low] <= arr[mid]){
+	            if(arr[low]<min){
+	                min = arr[low];
+	            }
+	            low = mid+1;
+	        }
+	        else{
+	            high = mid-1;
+	            if(arr[mid]<min){
+	                min = arr[mid];
+	            }
+	        }
+	        
+	    }
+	    
+	    System.out.println(min);
+	}
+}
