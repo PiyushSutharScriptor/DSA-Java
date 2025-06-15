dece1610b2fe4e9ffecaf0c6c06d344adf1332a4
Piyush <piyushsuthar6300@gmail.com>
2025-06-15 12:56:16 +0530

    Search element in rotated II (Binary Search)

--- Searching/BinarySearch/SearchElementInRotated2BinarySearch.java
+++ Searching/BinarySearch/SearchElementInRotated2BinarySearch.java
@@ -1,11 +1,11 @@
 
-public class SearchElementInRotatedBinarySearch
+public class SearchElementInRotated2BinarySearch
 {
 	public static void main(String[] args) {
-	    int arr[] = {7,8,9,1,2,3,4,5,6};
+	    int arr[] = {3,3,3,3,1,3,3,3};
 	    int n = arr.length;
 	    
-	    int target = 7  ;
+	    int target = 3222;
 	    
 	    int low = 0;
 	    int high = n-1;
@@ -15,11 +15,15 @@ public class SearchElementInRotatedBinarySearch
 	        int mid = (low+high)/2;
 	        
 	        if(arr[mid] == target){
-	            System.out.println("element found at index : " + mid);
 	            find = true;
+	            System.out.println(find);
 	            break;
 	        }
-	        if(arr[low] <= arr[mid]){
+	        else if(arr[low] == arr[mid] && arr[mid]== arr[high]){
+	            low++;
+	            high--;
+	        }
+	        else if(arr[low] <= arr[mid]){
 	            if(arr[low]<=target && target <= arr[mid]){
 	                high = mid-1;
 	            }
@@ -39,7 +43,7 @@ public class SearchElementInRotatedBinarySearch
 	    }
 	    
 	    if(!find){
-	    System.out.println("element not found");
+	    System.out.println(find);
 	    }
 	}
 }
