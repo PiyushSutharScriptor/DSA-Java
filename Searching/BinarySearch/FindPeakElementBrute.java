e226881d997b581bff4fe4f0187f21f364777339
Piyush <piyushsuthar6300@gmail.com>
2025-06-16 22:36:03 +0530

    Update FindPeakElementBrute.java

--- Searching/BinarySearch/FindPeakElementBrute.java
+++ Searching/BinarySearch/FindPeakElementBrute.java
@@ -7,6 +7,8 @@ public class FindPeakElementBrute
 	    int n = arr.length;
 	    
         //logic
+
+        //for peak on first element
 	    if(n==1 || arr[0]>arr[1]) System.out.println(arr[0]);
 	    for(int i=1 ; i<n-1 ; i++){
 	        if(arr[i-1]<arr[i] && arr[i]>arr[i+1]){
