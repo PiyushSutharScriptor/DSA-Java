f4421545771faeab58bd15a535fd7ec24544d6aa
Piyush <piyushsuthar6300@gmail.com>
2025-06-16 22:26:11 +0530

    Finding peak element (brute force)

---
+++ Searching/BinarySearch/FindPeakElementBrute.java
@@ -0,0 +1,18 @@
+
+public class FindPeakElementBrute
+{
+	public static void main(String[] args) {
+	    int arr[] = {2,5};
+	    
+	    int n = arr.length;
+	    
+	    if(n==1 || arr[0]>arr[1]) System.out.println(arr[0]);
+	    for(int i=1 ; i<n-1 ; i++){
+	        if(arr[i-1]<arr[i] && arr[i]>arr[i+1]){
+	            System.out.println(arr[i]);
+	        }
+	    }
+	    if(arr[n-1]>arr[n-2]) System.out.println(arr[n-1]);
+
+	}
+}
