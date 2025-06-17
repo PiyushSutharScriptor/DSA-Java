f4e120a17110e9a5bf515d68a848a20bbf14e127
Piyush <piyushsuthar6300@gmail.com>
2025-06-17 22:38:29 +0530

    Update PeakElementBSearch.java

--- Searching/BinarySearch/PeakElementBSearch.java
+++ Searching/BinarySearch/PeakElementBSearch.java
@@ -13,27 +13,26 @@ public class PeakElementBSearch
 	        System.out.println(arr[0]); 
 	        return;
 	    } 
-        if(arr[0]!=arr[1]){
-            System.out.println(arr[0]);
+        if(arr[0]>arr[1]){
+            System.out.println(arr[1]);
             return;
         } ;
-        if(arr[n-1]!=arr[n-2]){
+        if(arr[n-1]>arr[n-2]){
             System.out.println(arr[n-1]);
             return;
         }
             
 	    while(low <= high){
 	        int mid = (low+high)/2;
-	       // if(arr[mid]!=arr[mid-1] && arr[mid]!=arr[mid+1]) return arr[mid];
+	       
 	        if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
 	            System.out.println(mid);
-	            break;
 	        }
 	        else if(arr[mid]>arr[mid+1] && arr[mid]<arr[mid-1]){
-	            low = mid+1;
+				high = mid-1;
 	        }
 	        else{
-	            high = mid-1;
+	            low = mid+1;
 	        }
 	    }
 	}
