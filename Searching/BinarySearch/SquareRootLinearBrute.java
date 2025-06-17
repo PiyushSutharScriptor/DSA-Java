4ca6bd337136dcf7f4a7b24ba89f6df2edc2054d
Piyush <piyushsuthar6300@gmail.com>
2025-06-17 23:00:25 +0530

    Square root using for loop (better)

---
+++ Searching/BinarySearch/SquareRootLinearBrute.java
@@ -0,0 +1,18 @@
+
+public class SquareRootLinearBrute
+{
+	public static void main(String[] args) {
+	    int n = 25;
+	    int ans = 1;
+	    for(int i=0 ; i<n ; i++){
+	        if(i*i<=n){
+	            ans = i;
+	        }
+	        else{
+	            break;
+	        }
+	    }
+	    
+	    System.out.println(ans);
+	}
+}   
