9cab8e950bef72267f30cb0b1a3c418a4104a0c8
Piyush <piyushsuthar6300@gmail.com>
2025-06-18 21:59:28 +0530

    Koko eating banana (brute force)

---
+++ Searching/BinarySearch/KokoEatingBananaBrute.java
@@ -0,0 +1,27 @@
+
+public class KokoEatingBananaBrute
+{
+	public static void main(String[] args) {
+		int arr[] = {3,6,7,11};
+		int n = arr.length;
+		 
+		int banana = 1;
+		int maxtime = 8;
+		
+		for(int i=1 ; i<27 ; i++){
+		    
+		    int time = 0;
+    		for(int j=0 ; j<n ; j++){
+    		    time += arr[j]/i;
+    		}
+    		
+    		banana++;
+    		
+    		if(time == maxtime){
+    		    break;
+    		}
+		}
+		
+		System.out.println(banana);
+	}
+}
