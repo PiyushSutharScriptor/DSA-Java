ebbf9f4f3796b80d83c89f7400fe2fee025d25a0
Piyush <piyushsuthar6300@gmail.com>
2025-06-25 06:21:27 +0530

    Searching in 2D matrix (Binary Search)

---
+++ Searching/BinarySearch/SearchIn2DMatrixBSearch.java
@@ -0,0 +1,41 @@
+public class SearchIn2DMatrixBSearch
+{
+    public static boolean searchTarget(int arr[][] , int target, int m, int n){
+        
+        for(int i=0 ; i<m ;i++){
+            int low = 0;
+            int high = n-1;
+            while(low<high){
+                int mid = (low+high)/2;
+                if(arr[i][mid]==target){
+                    return true;
+                }
+                
+                if(arr[i][mid]<target){
+                    low = mid+1;
+                }
+                else{
+                    high = mid-1;
+                }
+            }
+        }
+        
+        return false;
+    }
+    
+	public static void main(String[] args) {
+	    int arr[][] = {
+	        {3,4,7,9},
+	        {12,13,16,18},
+	        {20,21,23,29}
+	    };
+	    
+	    int target = 444;
+
+	    int m = arr.length;
+	    int n = arr[0].length;
+        
+        System.out.println(searchTarget(arr,target,m,n));	    
+	    
+	}
+}
