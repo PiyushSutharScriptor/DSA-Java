322ff2a7562645dfd7196e3b4387ac82f9307e75
Piyush <piyushsuthar6300@gmail.com>
2025-06-28 10:36:30 +0530

    String input

---
+++ Strings/StringInput.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class StringInput
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    System.out.print("Enter number");
+	    String n = sc.nextLine();
+	    
+	    System.out.println(n);
+
+        sc.close();
+	}
+}
