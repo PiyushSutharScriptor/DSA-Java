ff0b8ee418a7cd9b0d7883c50abd455f23faddcd
Piyush <piyushsuthar6300@gmail.com>
2025-06-29 22:58:10 +0530

    Update IntegerToRoman.java

--- Strings/IntegerToRoman.java
+++ Strings/IntegerToRoman.java
@@ -8,6 +8,7 @@ public class IntegerToRoman
         int values[] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
         String symbols[] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         
+        //logic
         for(int i=0 ; i<values.length && n>0 ; i++){
             while(n>=values[i]){
                 n -= values[i];
