7afda0b483b2e1c62f3147d90e9d4759d3ada314
Piyush <piyushsuthar6300@gmail.com>
2025-07-03 18:47:18 +0530

    Insert element before tail of DLL (insert)

---
+++ LinkedList/InsertElementBeforeTailDLL.java
@@ -0,0 +1,63 @@
+class Node{
+    int data;
+    Node next;
+    Node back;
+
+    Node(int data1 , Node next1 , Node back1){
+        this.data = data1;
+        this.next = next1;
+        this.back = back1;
+    }
+
+    Node (int data2){
+        this.data = data2;
+        this.next = null;
+        this.back = null;
+    }
+}
+
+public class InsertElementBeforeTailDLL{
+
+    public static Node createDLL(int arr[]){
+        Node head = new Node(arr[0]);
+        Node prev = head;
+
+        for(int i=1 ; i<arr.length ; i++){
+            Node temp = new Node(arr[i],null,prev);
+            prev.next = temp;
+            prev = temp;
+        }
+
+        return head;
+    }
+
+    public static void printDLL(Node head){
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.data + " ");
+            temp= temp.next;
+        }
+    }
+
+    public static Node insertElBeforeT(Node head , int val){
+        Node temp = head;
+        while(temp.next!=null){
+            temp = temp.next;
+        }
+        Node prev = temp.back;
+        Node newNode = new Node(val,temp,prev);
+        prev.next = newNode;
+        temp.back = newNode;
+
+        return head;
+        
+    }
+    public static void main(String[] args) {
+        int arr[] = {10,20,30,40};
+        int val = 100;
+
+        Node head = createDLL(arr);
+        head = insertElBeforeT(head,val);
+        printDLL(head);
+    }
+}
