41d836b968e0dca711bab75066fca62052735bed
Piyush <piyushsuthar6300@gmail.com>
2025-07-05 15:06:04 +0530

    Length of cycle loop SLL brute (Hash Map)

---
+++ LinkedList/LengthOfCycleLoopSLLBrute.java
@@ -0,0 +1,67 @@
+import java.util.*;
+class Node{
+    int data;
+    Node next;
+
+    Node(int data1,Node next1){
+        this.data = data1;
+        this.next = next1;
+    }
+
+    Node(int data2){
+        this.data = data2;
+        this.next = null;
+    }
+}
+
+public class LengthOfCycleLoopSLLBrute {
+    
+    public static Node createLL(int arr[]){
+        Node head = new Node(arr[0]);
+        Node mover = head;
+
+        for(int i=1 ; i<arr.length ; i++){
+            Node temp = new Node(arr[i]);
+            mover.next = temp;
+            mover = temp;
+        }
+        
+        mover.next = head;
+
+        return head;
+    }
+
+    public static void printLL(Node head){
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.data+" ");
+            temp = temp.next;
+        }
+    }
+
+    public static int lengthOfCycleLoop(Node head) {
+        Node temp = head;
+        int count = 0;
+        
+        Map <Node, Integer> map = new HashMap<>();
+        
+        while(temp!=null){
+            map.put(temp, map.getOrDefault(temp , 0)+1);
+            if(map.get(temp)==2){
+                count++;
+            }
+            if(map.get(temp)==3){
+                return count;
+            }
+            temp = temp.next;
+        }
+        return -1;
+	}
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5};
+        Node head = createLL(arr);
+        int ans = lengthOfCycleLoop(head);
+        System.out.println(ans);
+    }
+}
