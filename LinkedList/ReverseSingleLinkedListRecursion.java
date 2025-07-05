48b4fa41d6f6bf10989b4ec294cd511999169d82
Piyush <piyushsuthar6300@gmail.com>
2025-07-05 11:51:42 +0530

    Update ReverseSingleLinkedListRecursion.java

--- LinkedList/ReverseSingleLinkedListRecursion.java
+++ LinkedList/ReverseSingleLinkedListRecursion.java
@@ -1,3 +1,63 @@
-public class ReverseSingleLinkedListRecursion {
-    
+import java.util.*;
+
+class Node{
+    int data;
+    Node next;
+    Node back;
+
+    Node(int data1 , Node next1 , Node back1){
+        this.data = data1;
+        this.next = next1;
+        this.back = back1;
+    }
+
+    Node (int data2){
+        this.data = data2;
+        this.next = null;
+        this.back = null;
+    }
+}
+
+public class ReverseSingleLinkedListRecursion{
+
+    public static Node createDLL(int arr[]){
+        Node head = new Node(arr[0]);
+        Node prev = head;
+
+        for(int i=1 ; i<arr.length ; i++){
+            Node temp = new Node(arr[i],null,prev);
+            prev.next = temp;
+            prev = temp;
+        }
+
+        return head;
+    }
+
+    public static void printDLL(Node head){
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.data + " ");
+            temp= temp.next;
+        }
+    }
+
+    public static Node reverseLL(Node head){
+        if(head==null || head.next == null){
+            return head;
+        }
+        Node newNode = reverseLL(head.next);
+        Node front = head.next;
+        front.next = head;
+        head.next = null;
+        return newNode;
+        
+    }
+    public static void main(String[] args) {
+        int arr[] = {10,20,30,40,50};
+
+        Node head = createDLL(arr);
+        head = reverseLL(head);
+        printDLL(head);
+        
+    }
 }
