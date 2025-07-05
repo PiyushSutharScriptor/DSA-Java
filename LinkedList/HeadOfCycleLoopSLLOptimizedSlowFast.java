fe4cae19375cf8f53836bee96fcf4bcca50be31d
Piyush <piyushsuthar6300@gmail.com>
2025-07-05 13:56:14 +0530

    Finding head of the cycle loop SLL optimized (slow and fast)

---
+++ LinkedList/HeadOfCycleLoopSLLOptimizedSlowFast.java
@@ -0,0 +1,72 @@
+import java.util.*;
+class Node{
+    int data;
+    Node next;
+
+    Node(int data1,Node next1){
+        this.data = data1;
+        this.next = next1;
+    }
+
+    Node(int data2){
+        this.data = data2;
+        this.next = null;
+    }
+}
+
+public class HeadOfCycleLoopSLLOptimizedSlowFast {
+    
+    public static Node createLL(int arr[]){
+        Node head = new Node(arr[0]);
+        Node mover = head;
+
+        for(int i=1 ; i<arr.length ; i++){
+            Node temp = new Node(arr[i]);
+            mover.next = temp;
+            mover = temp;
+        }
+        
+        mover.next = head;
+
+        return head;
+    }
+
+    public static void printLL(Node head){
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.data+" ");
+            temp = temp.next;
+        }
+    }
+
+    public static Node retrunHeadOfLoop(Node head) {
+        
+        if(head==null || head.next==null) return null;
+        
+        Node temp = head;
+		Node slow = head;
+		Node fast = head;
+		
+		while(fast!=null && fast.next!=null){
+		    slow = slow.next;
+		    fast = fast.next.next;
+		    
+		    if(slow == fast){
+		        slow = head;
+		        while(slow!=fast){
+		            slow = slow.next;
+		            fast = fast.next;
+		        }
+		        return slow;
+		    }
+		}
+		return null;
+	}
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5};
+        Node head = createLL(arr);
+        head = retrunHeadOfLoop(head);
+        System.out.println(head.data);
+    }
+}
