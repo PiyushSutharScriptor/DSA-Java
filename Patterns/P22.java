b2e01b425006f558055c3ec383889b9c29cd76d9
Piyush <piyushsuthar6300@gmail.com>
2025-07-11 22:39:38 +0530

    Update P22.java

--- Patterns/P22.java
+++ Patterns/P22.java
@@ -3,7 +3,7 @@ public class P22 {
         int n = 3;
 
         for (int i = 0; i < n; i++) {
-            int val = (i % 2 == 0) ? 1 : 0; // even rows start with 1, odd with 0
+            int val = (i % 2 == 0) ? 1 : 0; 
             for (int j = 0; j <= i; j++) {
                 System.out.print(val + " ");
                 val = 1 - val;
