94b80698511bfeb7821cd72ecb781d07caaafd03
Piyush <piyushsuthar6300@gmail.com>
2025-07-11 22:40:05 +0530

    Update P22.java

--- Patterns/P22.java
+++ Patterns/P22.java
@@ -3,6 +3,8 @@ public class P22 {
         int n = 3;
 
         for (int i = 0; i < n; i++) {
+
+            //when even = 0
             int val = (i % 2 == 0) ? 1 : 0; 
             for (int j = 0; j <= i; j++) {
                 System.out.print(val + " ");
