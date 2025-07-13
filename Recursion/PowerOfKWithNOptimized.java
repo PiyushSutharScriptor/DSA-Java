11d0fefd726b3c21f70810ce39a0a34065d58571
Piyush <piyushsuthar6300@gmail.com>
2025-07-13 21:35:33 +0530

    Power of k optimized (built in)

---
+++ Recursion/PowerOfKWithNOptimized.java
@@ -0,0 +1,9 @@
+public class PowerOfKWithNOptimized {
+    public static double powerOfK(int k, int n){
+        return Math.pow(k, n);
+    }
+	public static void main(String[] args) {
+		double n = powerOfK(3,3);
+		System.out.println(n);
+	}
+}
