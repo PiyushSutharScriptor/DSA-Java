298cc3381d6a175acbd93ca75981d47f53bb52fb
Piyush <piyushsuthar6300@gmail.com>
2025-07-19 21:49:19 +0530

    Word break optimized

---
+++ Recursion/WordBreakUsingDPOptimized.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class WordBreakUsingDPOptimized
+{
+	public static void main(String[] args) {
+
+	    List<String> wordDict = new ArrayList<>();
+	    wordDict.add("leet");
+	    wordDict.add("code");
+	    
+	    String s = "leetcode";
+	    
+	    Set<String> word = new HashSet<>(wordDict);
+	    
+	    boolean []b = new boolean[s.length()+1];
+	    
+	    b[0] = true;
+	    
+	    for(int i=1 ; i<=s.length() ; i++){
+	        for(int j=0 ; j<i ; j++){
+	            if(b[j] && word.contains(s.substring(j,i))){
+	                b[i]=true;
+	                break;
+	            }
+	        }
+	    }
+	    
+	    System.out.println(b[s.length()]);
+	    
+	}
+}
