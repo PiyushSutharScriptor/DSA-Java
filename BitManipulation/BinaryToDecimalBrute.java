3961f3438458e6e4617ed870e851769130db2fcb
Piyush <piyushsuthar6300@gmail.com>
2025-07-21 18:11:09 +0530

    Binary To Decimal Brute

---
+++ BitManipulation/BinaryToDecimalBrute.java
@@ -0,0 +1,20 @@
+package BitManipulation;
+
+public class BinaryToDecimalBrute
+{
+	public static void main(String[] args) {
+	    
+	    String s = "1101";
+	    
+	    int res = 0;
+        int n = s.length();
+        
+	    for(int i=s.length()-1 ; i>=0 ; i--){
+	        int digit = s.charAt(i)-'0';
+	        res += ((Math.pow(2,n-1-i)) * digit);
+	    }
+	    
+	    System.out.println(res);
+
+	}
+}
