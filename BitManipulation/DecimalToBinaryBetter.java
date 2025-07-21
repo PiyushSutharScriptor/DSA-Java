5e49dcca5a3f0e5ef759e935644b90f5d5ec38ed
Piyush <piyushsuthar6300@gmail.com>
2025-07-21 17:54:49 +0530

    Decimal to Binary (Better)

---
+++ BitManipulation/DecimalToBinaryBetter.java
@@ -0,0 +1,20 @@
+package BitManipulation;
+
+public class DecimalToBinaryBetter
+{
+	public static void main(String[] args) {
+	    
+	    int n = 13;
+	    StringBuilder s = new StringBuilder("");
+	    
+	    while(n!=0){
+	        int ans = n%2;
+	        char ch = (char)(ans+'0');
+	        s.append(ch);
+	        n=n/2;
+	    }
+	    
+	    System.out.println(s.reverse());
+
+	}
+}
