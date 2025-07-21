d3d03529acfe87c76bed0b5dcd8095f9b3acb23c
Piyush <piyushsuthar6300@gmail.com>
2025-07-21 18:11:18 +0530

    Binary To Decimal Better

---
+++ BitManipulation/BinaryToDecimalBetter.java
@@ -0,0 +1,19 @@
+package BitManipulation;
+
+public class BinaryToDecimalBetter
+{
+	public static void main(String[] args) {
+	    
+	    String s = "1101";
+	    
+	    int res = 0;
+
+	    for(int i=s.length()-1 ; i>=0 ; i--){
+	        int digit = s.charAt(i)-'0';
+	        res += ((Math.pow(2,s.length()-1-i)) * digit);
+	    }
+	    
+	    System.out.println(res);
+
+	}
+}
