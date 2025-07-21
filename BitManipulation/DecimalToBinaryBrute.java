cfec7d9859f01bb35587d41e1624a051a0ad1d09
Piyush <piyushsuthar6300@gmail.com>
2025-07-21 17:54:35 +0530

    Decimal to Binary (brute)

---
+++ BitManipulation/DecimalToBinaryBrute.java
@@ -0,0 +1,19 @@
+package BitManipulation;
+
+public class DecimalToBinaryBrute
+{
+	public static void main(String[] args) {
+	    
+	    int n = 13;
+        StringBuilder s = new StringBuilder("");
+        
+	    while(n!=0){
+	        if(n%2==0) s.append('0');
+	        else s.append('1');
+	        n=n/2;
+	    }
+	    
+	    System.out.println(s.reverse());
+
+	}
+}
