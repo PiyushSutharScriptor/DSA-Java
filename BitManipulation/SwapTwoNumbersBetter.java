39668be100551d50a7543899cfc792e4aceba06b
Piyush <piyushsuthar6300@gmail.com>
2025-07-22 17:28:52 +0530

    Swap two numbers better

---
+++ BitManipulation/SwapTwoNumbersBetter.java
@@ -0,0 +1,13 @@
+public class SwapTwoNumbersBetter {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        System.out.println("Before : " + "a : " + a + " b : " + b);
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After : " + "a : " + a + " b : " + b);
+    }
+}
