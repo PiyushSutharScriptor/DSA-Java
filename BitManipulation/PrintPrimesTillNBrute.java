14c960f6699334be992404a43b86e2265c6f2996
Piyush <piyushsuthar6300@gmail.com>
2025-07-26 23:08:07 +0530

    Print prime numbers till n (brute)

---
+++ BitManipulation/PrintPrimesTillNBrute.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class PrintPrimesTillNBrute {
+    public static void main(String[] args) {
+        int n = 15;
+
+        List<Integer> li = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (i == 1)
+                continue;
+            else if (i == 2)
+                li.add(i);
+            else {
+                boolean isPrime = true;
+                for (int j = 2; j <= Math.sqrt(i); j++) {
+                    if (i % j == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                    li.add(i);
+            }
+        }
+        System.out.println(li);
+    }
+
+}
