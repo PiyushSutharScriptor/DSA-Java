027a7010102b9d88a733d7b04a56594f3d6651d2
Piyush <piyushsuthar6300@gmail.com>
2025-07-30 22:13:32 +0530

    Min stack (brute)

---
+++ StacksAndQueue/MinStackBrute.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class MinStackBrute{
+    
+    private Stack<Integer> st = new Stack<>();
+    
+    public void push(int val) {
+        st.push(val);
+    }
+    
+    public void pop() {
+        st.pop();
+    }
+    
+    public int top() {
+        return st.peek();
+    }
+    
+    public int getMin() {
+        int min = Integer.MAX_VALUE;
+
+        for(int i=0 ; i<st.size() ; i++){
+            if(st.elementAt(i)<min) min = st.elementAt(i);
+        }        
+
+        return min;
+    }
+    public static void main(String[] args) {
+        MinStackBrute s1 = new MinStackBrute();
+        s1.push(10);
+        s1.push(20);
+        s1.push(30);
+
+        System.out.println(s1.getMin());
+    }
+}
