dbd8c39ba70e3c50416df3eedbe73954c60cda8c
Piyush <piyushsuthar6300@gmail.com>
2025-07-31 22:52:18 +0530

    Postfix to Infix

---
+++ StacksAndQueue/PostfixToInfix.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class PostfixToInfix
+{
+	public static void main(String[] args) {
+	    String s = "AB-DE+F*/";
+	    
+	    Stack<String> st = new Stack<>();
+	    
+	    for(int i=0 ; i<s.length() ; i++){
+	        char ch = s.charAt(i);
+	        
+	        if((ch>='a' && ch<='z') ||
+	           (ch>='A' && ch<='Z') || 
+	           (ch>='0' && ch<='9')
+	            ){
+	            st.push(Character.toString(ch));
+	        }
+	        else{
+	            String a = st.pop();
+	            String b = st.pop();
+	            
+	            String c = "("+a+ch+b+")";
+	            st.push(c);
+	        }
+	    }
+	    
+	    for(String str : st){
+	        System.out.println(str);
+	    }
+
+	}
+}
