42627ff47e2f1569a412033230774ec28f17215e
Piyush <piyushsuthar6300@gmail.com>
2025-08-01 21:22:08 +0530

    Next greater element brute

---
+++ StacksAndQueue/NextGreaterElementBrute.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class NextGreaterElementBrute {
+    public static void main(String[] args) {
+
+      int arr[] = {6,0,8,1,3};
+      int n = arr.length;
+      
+      for(int i=0 ; i<n-1 ; i++){
+        boolean found = false;
+        for(int j=i+1 ; j<n ; j++){
+          if(arr[i]<arr[j]){
+            arr[i]=arr[j];
+            found=true;
+            break;
+          } 
+        }
+        if(!found){
+          arr[i]=-1;
+        }
+      }
+      
+      arr[n-1] = -1;
+      
+      for(int e : arr){
+        System.out.println(e);
+      }
+  }
+}
