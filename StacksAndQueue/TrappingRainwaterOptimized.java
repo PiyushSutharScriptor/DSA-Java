f8cfa96c3ab04c8f2a8dff3d2dd5b550a5b18474
Piyush <piyushsuthar6300@gmail.com>
2025-08-03 12:11:46 +0530

    Trapping rain water optimized

---
+++ StacksAndQueue/TrappingRainwaterOptimized.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class TrappingRainwaterBetterUsingArray
+{
+	public static void main(String[] args) {
+	    int arr[] = {0,1,0,2,1,0,1,3,2,1,2,1};
+	    int n = arr.length;
+	    
+	    int count=0;
+	    
+	    int lmax=0, rmax=0, total=0;
+	    
+	    int l=0;
+	    int r=n-1;
+	    
+	    while(l<r){
+	        if(arr[l]<=arr[r]){
+	            if(lmax>arr[l]){
+	                count+=lmax-arr[l];
+	            }
+	            else lmax = arr[l];
+	            l++;
+	        }
+	        else{
+	            if(rmax>arr[r]){
+	                count+=rmax-arr[r];
+	            }
+	            else rmax = arr[r];
+	            r--;
+	        }
+	    }
+	    
+	    System.out.println(count);
+	}
+}
