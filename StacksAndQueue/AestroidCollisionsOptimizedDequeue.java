89de173ecf0dab3ea46c32c8f6776da3593e490e
Piyush <piyushsuthar6300@gmail.com>
2025-08-03 23:07:20 +0530

    Aestroid collisions optimized using stack

---
+++ StacksAndQueue/AestroidCollisionsOptimizedDequeue.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class AestroidCollisionsOptimizedDequeue
+{
+	public static void main(String[] args) {
+	    int arr[] = {-2,-1,1,2};
+	    
+	    int n = arr.length;
+	    Deque<Integer> st = new ArrayDeque<>();
+	    
+	    for(int i=0 ; i<n ; i++){
+	            if(arr[i]>0) st.push(arr[i]);
+	            else{
+    	            while(!st.isEmpty() && st.peek()<-arr[i] && st.peek()>0){
+    	                st.pop();
+    	            }
+    	            if(st.isEmpty()){
+    	                st.push(arr[i]);
+    	            }
+    	            else if(st.peek()==-arr[i]) st.pop();
+    	            else st.push(arr[i]);
+	            }
+	    }
+	    
+	    System.out.println(st);
+	    
+	}
+}
