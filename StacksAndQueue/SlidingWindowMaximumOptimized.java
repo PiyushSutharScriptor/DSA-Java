1c238a70513defebd2b6a4b10086919e41fbc837
Piyush <piyushsuthar6300@gmail.com>
2025-08-07 22:57:51 +0530

    Sliding window maximum optimized

---
+++ StacksAndQueue/SlidingWindowMaximumOptimized.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class SlidingWindowMaximumOptimized {
+    public static void main(String[] args) {
+        int arr[] = {1,3,-1,-3,5,3,2,1,6};
+        int k = 3;
+
+        int n = arr.length;
+
+        List<Integer> li = new ArrayList<>();
+        Deque<Integer> deq = new ArrayDeque<>();
+
+        for(int i=0 ; i<n ; i++){
+            if(!deq.isEmpty() && deq.peekFirst()<=i-k){
+                deq.removeFirst();
+            }
+
+            while(!deq.isEmpty() && arr[deq.peekLast()]<=arr[i]){
+                deq.removeLast();
+            }
+
+            deq.addLast(i);
+
+            if(i>=k-1){
+                li.add(arr[deq.peekFirst()]);
+            }
+        }
+
+        System.out.println(li);
+
+    }
+}
