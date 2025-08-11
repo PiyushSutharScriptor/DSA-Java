a32543bfb98caf80bbdc2dbc9c54760f1cd6784e
Piyush <piyushsuthar6300@gmail.com>
2025-08-11 18:07:03 +0530

    Create MaxConsecutiveOnesWithKZeroBetter.java

---
+++ SlidingWindowAndTwoPointer/MaxConsecutiveOnesWithKZeroBetter.java
@@ -0,0 +1,29 @@
+public class MaxConsecutiveOnesWithKZeroBetter {
+    public static void main(String[] args) {
+        int arr[] = {1,1,1,0,0,0,1,1,1,1};
+        int n = arr.length;
+        int target = 2;
+
+        int count = 0;
+
+        for(int i=0 ; i<n ; i++){
+
+            int countz = 0;
+            for(int j=i ; j<n ; j++){
+
+                if(arr[j]==0) countz++;
+
+                if(countz<=target){
+                    int len = j-i+1;
+                    count = Math.max(len,count);
+                }
+                else{
+                    break;
+                }
+
+            }
+        }
+
+        System.out.println(count);
+    }
+}
