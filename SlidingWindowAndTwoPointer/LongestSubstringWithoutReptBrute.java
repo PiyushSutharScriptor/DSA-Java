1315c42d4d22062af7a61a09e054714e4c4b7c42
Piyush <piyushsuthar6300@gmail.com>
2025-08-11 05:58:47 +0530

    Longest subarray without  repeat brute

---
+++ SlidingWindowAndTwoPointer/LongestSubstringWithoutReptBrute.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class LongestSubstringWithoutReptBrute {
+    public static void main(String[] args) {
+        String s = "abcd";
+
+        int n = s.length();
+        List<String> li = new ArrayList<>();
+
+        int length=0;
+        for(int i=0 ; i<n ; i++){
+            int arr[] =new int[255];
+            for(int j=i ; j<n ; j++){
+                if(arr[s.charAt(j)]==1) break;
+                length = Math.max(length,j-i+1);
+                arr[s.charAt(j)]=1;
+            }
+        }
+
+        System.out.println(length);
+    }
+}
