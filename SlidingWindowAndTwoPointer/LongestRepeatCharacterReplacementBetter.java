8bad8a19c0c63613781b639b4a15a78b9b441a37
Piyush <piyushsuthar6300@gmail.com>
2025-08-13 17:53:00 +0530

    Longest repeat character replacement better

---
+++ SlidingWindowAndTwoPointer/LongestRepeatCharacterReplacementBetter.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class LongestRepeatCharacterReplacementBetter {
+    public static void main(String[] args) {
+        String s = "AAABABBA";
+        int k = 2;
+
+        int n = s.length();
+        int maxf = 0;
+        int count = 0;
+
+        int l=0;
+        int arr[] = new int[26];
+
+        for(int r=0 ; r<n ; r++){
+            arr[s.charAt(r)-'A']++;
+            maxf = Math.max(maxf,arr[s.charAt(r)-'A']);
+
+            while((r-l+1)-maxf>k){
+                arr[s.charAt(l)-'A']--;
+                maxf = Math.max(maxf, arr[s.charAt(l)-'A']);
+                l++;
+            }
+
+            int len = (r-l+1)-maxf;
+            if(len<=k){
+                count = Math.max(count,r-l+1);
+            }   
+
+        }
+
+        System.out.println(count);
+    }
+}
