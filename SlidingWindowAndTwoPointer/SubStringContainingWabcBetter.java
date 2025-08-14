67e91b8b6b613e83237c0cab8330b189d1850269
Piyush <piyushsuthar6300@gmail.com>
2025-08-14 23:16:49 +0530

    Substring containing a,b,c better

---
+++ SlidingWindowAndTwoPointer/SubStringContainingWabcBetter.java
@@ -0,0 +1,20 @@
+public class SubStringContainingWabcBetter {
+    public static void main(String[] args) {
+        String s = "abcabc";
+        int n = s.length();
+
+        int count = 0;
+        for(int i=0 ; i<n ; i++){
+            int arr[] = new int[3];
+            for(int j=i ; j<n ; j++){
+                arr[s.charAt(j)-'a']=1;
+                if(arr[0]+arr[1]+arr[2]==3){
+                    count+=(n-j);
+                    break;
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}
