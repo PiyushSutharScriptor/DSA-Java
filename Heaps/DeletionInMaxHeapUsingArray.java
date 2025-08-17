7a6f5e9f285ad689ac5045221c2b0a7624e4d272
Piyush <piyushsuthar6300@gmail.com>
2025-08-17 16:05:22 +0530

    Create DeletionInMaxHeapUsingArray.java

---
+++ Heaps/DeletionInMaxHeapUsingArray.java
@@ -0,0 +1,85 @@
+
+class Heap{
+    private int arr[];
+    private int size;
+
+    Heap(int len){
+        arr = new int[len+1];
+        size = 0;
+    }
+
+    public void insert(int value){
+        size++;
+        arr[size] = value;
+
+        int idx = size;
+
+        while(idx>1){
+            if(arr[idx/2]<arr[idx]){
+                swap(idx/2, idx);   
+            }
+            else{
+                return;
+            }
+
+            idx/=2;
+        }
+    }
+
+    //deleting root node
+    public void delete(){
+        if(size==0){
+            System.out.println("Nothing to delete");
+            return;
+        }
+
+        arr[1] = arr[size];
+        arr[size] = 0;
+
+        int idx = 1;
+        while (idx<size) {
+            int left = 2*idx;
+            int right = 2*idx+1;
+            if(left<size && arr[left]>arr[idx]){
+                swap(idx, left);
+            }
+            else if(right<size && arr[right]>arr[idx]){
+                swap(idx, right);
+            }
+            else return;
+
+            idx++;
+        }
+    }
+
+    public void swap(int a, int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+    public void printHeap(){
+        for(int i=1 ; i<=size ; i++){ //option i<=arr.length;
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
+
+public class DeletionInMaxHeapUsingArray {
+    public static void main(String[] args) {
+        Heap h1 = new Heap(5);
+
+        h1.insert(10);
+        h1.insert(20);
+        h1.insert(30);
+        h1.insert(40);
+
+        h1.printHeap();
+
+        System.out.println();
+        
+        h1.delete();
+        h1.printHeap();
+
+    }    
+}
