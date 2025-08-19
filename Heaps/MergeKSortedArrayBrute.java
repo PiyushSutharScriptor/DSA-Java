b61331d023ba5725412c864bf31115b1536bbe20
Piyush <piyushsuthar6300@gmail.com>
2025-08-19 23:24:35 +0530

    Merge K sorted array brute

---
+++ Heaps/MergeKSortedArrayBrute.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class MergeKSortedArrayBrute {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      
+      int t = sc.nextInt();
+      sc.nextLine();
+      
+      while(t!=0){
+        int n = sc.nextInt();
+        List<Integer> li = new ArrayList<>();
+        
+        for(int i=0 ; i<n ; i++){
+          int size = sc.nextInt();
+          for(int j=0 ; j<size ; j++){
+            int x = sc.nextInt();
+            li.add(x);
+          }
+        }
+        
+        Collections.sort(li);
+        
+        System.out.println(li);
+        
+        t--; 
+      }
+  }
+}
