9925ea0f26ab85020c4b5858da3cc9e416a013c0
Piyush <piyushsuthar6300@gmail.com>
2025-08-21 23:11:32 +0530

    Divide the array into k groups

---
+++ Heaps/DivideTheArrayIntoKGruops.java
@@ -0,0 +1,43 @@
+import java.util.Map;
+import java.util.TreeMap;
+
+public class DivideTheArrayIntoKGruops {
+    public static boolean findGroups(int arr[] , int group){
+        int n = arr.length;
+
+        if(n%group!=0) return false;
+        
+        Map<Integer , Integer> map = new TreeMap<>();
+
+        for(int el : arr){
+            map.put(el,map.getOrDefault(el, 0)+1);
+        }
+
+        while(!map.isEmpty()){
+            int first = map.keySet().iterator().next();
+
+            for(int i=0 ; i<group ; i++){
+                int card = first + i;
+
+                if(!map.containsKey(card)){
+                    return false;
+                }
+
+                map.put(card, map.get(card)-1);
+                if(map.get(card)==0){
+                    map.remove(card);
+                }
+            }
+
+        }
+
+        return true;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,6,2,3,4,7,8};
+        int group = 3;
+
+        System.out.println(findGroups(arr, group));
+
+    }
+}
