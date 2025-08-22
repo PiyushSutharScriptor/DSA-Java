eb662ca1b3fe391ce293e2a148591084dd026381
Piyush <piyushsuthar6300@gmail.com>
2025-08-23 00:04:11 +0530

    Connect n ropes with minimal cost brute

---
+++ Heaps/ConnectNRopesWithMinimalCostBrute.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class ConnectNRopesWithMinimalCostBrute {
+    public static void main(String[] args) {
+        int arr[] = {2,4,3};
+
+        int n = arr.length;
+
+        Arrays.sort(arr);
+
+        int sum = arr[0];
+        int res = 0;
+        for(int i=1 ; i<n ; i++){
+            sum+=arr[i];
+            res+=sum;
+        }
+
+        System.out.println(res);
+    }
+}
