faf1be5707788763f13965bdbf31f2bb9c8ff154
Piyush <piyushsuthar6300@gmail.com>
2025-08-28 23:50:22 +0530

    Dfs PreOrder traversal

---
+++ BinaryTree/DfsPreOrderTraversal.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Node{
+    Node left;
+    Node right;
+    int data;
+
+    Node(int data1){
+        this.data = data1;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class DfsPreOrderTraversal {
+    public static void pre(Node root, List<Integer> li){
+        if(root==null){
+            return;
+        }
+        li.add(root.data);
+        pre(root.left , li);
+        pre(root.right , li);
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(2);
+        root.left = new Node(3);
+        root.right = new Node(4);
+        root.left.left = new Node(5);
+        root.left.right = new Node(6);
+        root.right.left = new Node(7);
+
+        List<Integer> li = new ArrayList<>();
+        pre(root, li);
+
+        System.out.println(li);
+    }
+
+}
