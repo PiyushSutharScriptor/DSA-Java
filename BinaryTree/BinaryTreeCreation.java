a5e6d9c0837b1e6dcf4b3fb00ade929cc7b20412
Piyush <piyushsuthar6300@gmail.com>
2025-08-28 21:55:54 +0530

    Binary tree creation

---
+++ BinaryTree/BinaryTreeCreation.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Node{
+    Node left;
+    Node right;
+    int data;
+
+    Node(int data1){
+        this.data = data1;
+        this.left = null;
+        this.right = null;
+    }
+}
+public class BinaryTreeCreation{
+    public static void main(String[] args) {
+        Node root = new Node(2);
+        root.left = new Node(3);
+        root.right = new Node(4);
+        root.left.left = new Node(5);
+        root.left.right = new Node(6);
+        root.right.left = new Node(7);
+    }
+}
