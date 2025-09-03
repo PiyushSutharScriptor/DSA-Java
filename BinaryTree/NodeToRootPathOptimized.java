87c417b8478800571802750704c461e85f6304f7
Piyush <piyushsuthar6300@gmail.com>
2025-09-03 23:41:34 +0530

    Root to Node path optimized

---
+++ BinaryTree/NodeToRootPathOptimized.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data1){
+        this.data = data1;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+
+public class NodeToRootPathOptimized{
+
+    public static boolean isPossible(Node root, int target, List<Integer> li){
+        if(root==null) return false;
+
+        li.add(root.data);
+
+        if(root.data==target) return true;
+
+        if(isPossible(root.left, target, li) || isPossible(root.right, target, li)){
+            return true;
+        }
+
+        li.remove(li.size()-1);
+        return false;
+
+    }
+
+    public static List<Integer> findPath(Node root, int target){
+        ArrayList<Integer> li = new ArrayList<>();
+
+        if(root==null){
+            return li;
+        }
+        isPossible(root, target, li);
+        return li;
+    }
+
+    public static void main(String[] args){
+        
+        Node root = new Node(2);
+        root.left = new Node(3);
+        root.right = new Node(4);
+        root.left.left = new Node(5);
+        root.left.right = new Node(6);
+        root.right.left = new Node(7);
+
+        int target = 7;
+
+        List<Integer> res = findPath(root , target);
+
+        Collections.reverse(res);
+        System.out.println(res);
+    }
+}
