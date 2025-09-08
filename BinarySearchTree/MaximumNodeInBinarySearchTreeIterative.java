08b5477e8994673bbdb4f94bf0fba67e0dc826c0
Piyush <piyushsuthar6300@gmail.com>
2025-09-08 17:55:34 +0530

    Maximum node in BST iterative

---
+++ BinarySearchTree/MaximumNodeInBinarySearchTreeIterative.java
@@ -0,0 +1,37 @@
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data1) {
+        this.data = data1;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class MaximumNodeInBinarySearchTreeIterative{
+    public static Node searchMaxNode(Node root){
+        if(root == null) return null;
+
+        while(root.right!=null){
+            root = root.right;
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(40);
+        root.left = new Node(20);
+        root.right = new Node(60);
+        root.left.left = new Node(10);
+        root.left.right = new Node(30);
+        root.right.left = new Node(50);
+        root.right.right = new Node(70);
+
+        Node ans = searchMaxNode(root);
+        System.out.println(ans.data);
+    }
+}
+
