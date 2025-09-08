b824ba691460821a83fa44acc3a315ba881cc3a0
Piyush <piyushsuthar6300@gmail.com>
2025-09-08 17:55:15 +0530

    Search node in a binary tree optimized

---
+++ BinarySearchTree/SearchInBinarySearchTreeOptimized.java
@@ -0,0 +1,41 @@
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data1) {
+        this.data = data1;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class SearchInBinarySearchTreeOptimized {
+    public static Node searchNode(Node root, int target){
+        while(root!=null && root.data!=target){
+            root = target<root.data ? root.left : root.right;
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(40);
+        root.left = new Node(20);
+        root.right = new Node(60);
+        root.left.left = new Node(10);
+        root.left.right = new Node(30);
+        root.right.left = new Node(50);
+        root.right.right = new Node(70);
+
+        int target = 70;
+
+        Node ans = searchNode(root, target);
+        System.out.println(ans.data); //for !null directly print ans.data or else handle
+                                        // NullPointerException by using 
+                                        /*
+                                            if(ans!=null) sout(ans.data)
+                                            else sout("not found")
+                                        */
+    }
+}
+
