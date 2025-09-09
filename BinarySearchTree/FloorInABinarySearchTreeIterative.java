4047294486d8a5001b7fe3d6d154f9b1118f44ef
Piyush <piyushsuthar6300@gmail.com>
2025-09-09 17:42:00 +0530

    Floor ceil in BST iterative

---
+++ BinarySearchTree/FloorInABinarySearchTreeIterative.java
@@ -0,0 +1,50 @@
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data1) {
+        this.data = data1;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class FloorInABinarySearchTreeIterative {
+
+    public static int findFloor(Node root, int key){
+        if(root==null) return 0;
+
+        int floor = -1;
+
+        while(root!=null){
+            if(root.data<=key) floor = root.data;
+
+            if(root.data>key){
+                root = root.left;
+            }
+            else{
+                root = root.right;
+            }
+        }
+
+        return floor;
+        
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(40);
+        root.left = new Node(20);
+        root.right = new Node(60);
+        root.left.left = new Node(10);
+        root.left.right = new Node(30);
+        root.right.left = new Node(50);
+        root.right.right = new Node(70);
+
+        int key = 52;
+
+        int res = findFloor(root, key);
+        System.out.println(res);
+    }
+}
+
