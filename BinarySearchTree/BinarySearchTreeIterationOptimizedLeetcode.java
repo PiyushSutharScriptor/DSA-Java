4c6a7b7c8632531981491a1a2703548d542b60f5
Piyush <piyushsuthar6300@gmail.com>
2025-09-14 17:28:18 +0530

    BST iterator next leetcode

---
+++ BinarySearchTree/BinarySearchTreeIterationOptimizedLeetcode.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+import java.util.*;
+
+class BSTIterator {
+    Stack<TreeNode> st = new Stack<>();
+
+    public BSTIterator(TreeNode root) {
+        insertAll(root);
+    }
+
+    public void insertAll(TreeNode root){
+        if(root==null) return;
+        st.push(root);
+        insertAll(root.left);
+    }
+    
+    
+    public int next() {
+        TreeNode n = st.pop();
+        insertAll(n.right);
+        return n.val;
+    }
+    
+    public boolean hasNext() {
+        return !st.isEmpty();
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
