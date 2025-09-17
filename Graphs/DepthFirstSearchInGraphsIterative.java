3c6e449672a80d234f20d42f1d71f6ca3e30ff48
Piyush <piyushsuthar6300@gmail.com>
2025-09-17 18:37:56 +0530

    DFS in graphs (Iterative)

---
+++ Graphs/DepthFirstSearchInGraphsIterative.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+public class DepthFirstSearchInGraphsIterative{
+
+    public static List<Integer> findDfs(int[][]edges, int v, int k){
+        List<List<Integer>> li = new ArrayList<>();
+
+        for(int i=0 ; i<v ; i++){
+            li.add(new ArrayList<>());
+        }
+
+        for(int[] edge : edges){
+            int U = edge[0];
+            int V = edge[1];
+
+            li.get(U).add(V);
+            li.get(V).add(U);
+        }
+
+        boolean visited[] = new boolean[v];
+        List<Integer> res = new ArrayList<>();
+        Stack<Integer> st = new Stack<>();
+
+        st.push(k);
+
+        while(!st.isEmpty()){
+            int node = st.pop();
+            if(!visited[node]){
+                visited[node] = true;
+                res.add(node);
+            }
+
+            for(int el : li.get(node)){
+                if(!visited[el]){
+                    st.add(el);
+                }
+            }
+        }
+        
+
+        return res;
+    }
+
+    public static void dfs(int node, List<List<Integer>> li, List<Integer> res, boolean[] visited){
+        visited[node] = true;
+        res.add(node);
+
+        for(int el : li.get(node)){
+            if(!visited[el]){
+                dfs(el,li,res,visited);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int v = 5;
+        int edges[][] = { { 0, 1 }, { 0, 2 }, { 1, 3 }, { 2, 4 } };
+        int k = 0;
+
+        List<Integer> res = findDfs(edges,v,k);
+        System.out.println(res);
+    }
+}
