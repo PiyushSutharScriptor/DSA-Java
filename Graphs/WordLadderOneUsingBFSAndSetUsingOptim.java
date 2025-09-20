77b3dd2f084961c243c4c050eb17f6a939eea3eb
Piyush <piyushsuthar6300@gmail.com>
2025-09-20 19:00:37 +0530

    Word ladder using BFS and set optimized

---
+++ Graphs/WordLadderOneUsingBFSAndSetUsingOptim.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+class Pair{
+    String str;
+    int len;
+
+    Pair(String str1, int len1){
+        this.str = str1;
+        this.len = len1;
+    }
+}
+public class WordLadderOneUsingBFSAndSetUsingOptim{
+
+    public static int findCount(List<String> originalList, String start, String end){
+
+        Set<String> wordList = new HashSet<>();
+
+        for(String se : originalList){
+            wordList.add(se);
+        }
+
+        Queue<Pair> que = new LinkedList<>();
+        que.add(new Pair(start, 1));
+
+        wordList.remove(start);
+
+        while(!que.isEmpty()){
+            Pair p = que.poll();
+            String word = p.str;
+            int len = p.len;
+
+            // when reaches the target
+            if(word.equals(end)==true) return len;
+
+            for(int i=0 ; i<word.length() ; i++){
+                for(char ch='a' ; ch<='z' ; ch++){
+                    char[] charArray = word.toCharArray();
+                    charArray[i] = ch;
+
+                    String str = new String(charArray);
+
+                    if(wordList.contains(str)==true){
+                        wordList.remove(str);
+                        que.add(new Pair(str, len+1));
+                    }
+                }
+            }
+
+        }
+
+        return 0;
+    }
+    public static void main(String[] args) {
+        String[] arr = {"hot","dot","dog","lot","log","cog"};
+        String start= "hit";
+        String end = "cog";
+
+        List<String> wordList = new ArrayList<>();
+
+        for(String s : arr){
+            wordList.add(s);
+        }
+
+        System.out.println(findCount(wordList,start,end));
+
+    }
+}
+
