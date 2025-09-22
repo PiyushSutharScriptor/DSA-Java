04723b40ba0861ad40c33a7702ae144e7818aa41
Piyush <piyushsuthar6300@gmail.com>
2025-09-22 22:11:41 +0530

    Topological sort using DFS

---
+++ Graphs/TopologicalSortUsingDFS.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+public class TopologicalSortUsingDFS {
+
+    public static int[] findOrder(int[][] edges, int n){
+
+        List<List<Integer>> li = new ArrayList<>();
+
+        for(int i=0 ; i<n ; i++){
+            li.add(new ArrayList<>());
+        }
+
+        for(int[]edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+
+            li.get(u).add(v);
+        }
+
+        boolean[] visited = new boolean[n];
+        Stack<Integer> st = new Stack<>();
+
+        for(int i=0 ; i<n ; i++){
+            if(!visited[i]){
+                dfs(i,visited,edges,st,li);
+            }
+        }
+
+        int arr[] = new int[st.size()];
+        int i=0;
+
+        while(!st.isEmpty()){
+            arr[i++] = st.pop();
+        }
+
+        return arr;
+    }
+
+    public static void dfs(int node,boolean[] visited,int[][] edges,Stack<Integer> st,List<List<Integer>> li){
+        visited[node] = true;
+
+        for(int el : li.get(node)){
+            if(!visited[el]){
+                dfs(el, visited, edges, st, li);
+            }
+        }
+
+        st.push(node);
+    }
+
+    public static void main(String[] args) {
+        int edges[][] = {{5,0},{4,0},{5,2},{2,3},{3,1},{4,1}};
+        int v = 6;
+
+        int res[] = findOrder(edges,v);
+
+        for(int el : res){
+            System.out.print(el + " ");
+        }
+    }
+}
