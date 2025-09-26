1b7fa0a27a0182bc0d6a660628c991c579b7ee15
Piyush <piyushsuthar6300@gmail.com>
2025-09-26 21:37:36 +0530

    Dijkstra algorithm using set

---
+++ Graphs/DijkstraAlgorithmUsingSet.java
@@ -0,0 +1,77 @@
+import java.util.*;
+
+class Pair{
+    int node;
+    int dist;
+
+    Pair(int dist1, int node1){
+        this.dist = dist1;
+        this.node = node1;
+    }
+}
+
+
+public class DijkstraAlgorithmUsingSet {
+
+    public static int[] findDijkstra(int grid[][], int n, int v, int k){
+
+        List<List<Pair>> li = new ArrayList<>();
+
+        for(int i=0 ; i<v ; i++){
+            li.add(new ArrayList<>());
+        }
+
+        for(int[] edge : grid){
+            int U = edge[0];
+            int V = edge[1];
+            int D = edge[2];
+
+            li.get(U).add(new Pair(D, V));
+        }
+
+        TreeSet<Pair> set = new TreeSet<>((a,b)->{
+            if(a.dist==b.dist) return a.node-b.node;
+            return a.dist-b.dist;
+        });
+        
+        set.add(new Pair(0,k));
+
+        int[] res = new int[v];
+        Arrays.fill(res,Integer.MAX_VALUE);
+
+        res[k] = 0;
+
+        while(!set.isEmpty()){
+            Pair p = set.pollFirst();
+            int node = p.node;
+            int dist = p.dist;
+
+            for(Pair pr : li.get(node)){
+                if(res[pr.node]>pr.dist+dist){
+
+                    set.remove(new Pair(res[pr.node], pr.node));
+                    res[pr.node] = pr.dist+dist;
+                    set.add(new Pair(pr.dist+dist, pr.node));
+                }
+            }
+        }
+
+        return res;
+
+    }
+
+    public static void main(String[] args) {
+        int grid[][] = {{0,1,4},{0,2,4},{1,0,4},{1,2,2},{2,0,4},{2,1,2},{2,3,3},{2,4,1},{2,5,6},{3,2,3},{3,5,2},{4,2,1},{4,5,3},{5,2,6},{5,3,2},{5,4,3}};
+
+        int n = grid.length;
+        int v = 6;
+        int k = 0;
+
+        int[] res = findDijkstra(grid,n,v,k);
+
+        for(int el : res){
+            System.out.print(el + " ");
+        }
+    }
+}
+
