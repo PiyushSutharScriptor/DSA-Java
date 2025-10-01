2e3fc454a4564b657b5dde3c1b983579347d0992
Piyush <piyushsuthar6300@gmail.com>
2025-10-01 22:47:39 +0530

    Detect cycle using bellman algo optimized

---
+++ Graphs/DetectNegaCycleUsingBellmanAlgoOptimized.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class DetectNegaCycleUsingBellmanAlgoOptimized{
+    public static boolean findCycle( int v, int[][] edges, int src){
+
+        int n = edges.length;
+
+        int adj[] = new int[v];
+        Arrays.fill(adj, Integer.MAX_VALUE);
+        adj[src] = 0;
+
+        for(int i=0 ; i<v ; i++){
+
+            for(int j=0 ; j<n ; j++){
+                int U = edges[j][0];
+                int V = edges[j][1];
+                int D = edges[j][2];
+                
+                if(adj[U]!=Integer.MAX_VALUE && adj[V]>adj[U]+D){
+                    adj[V] = adj[U]+D;
+                    if(i==v-1) return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int v = 6;
+        int[][] edges = {{3, 2, 6}, {5, 3, 1}, {0, 1, 5}, {1, 5, -3}, {1, 2, -2}, {3, 4,-2}, {2, 4, 3}};
+        int src = 0;
+
+        System.out.println(findCycle(v,edges,src));
+    }
+}
+
