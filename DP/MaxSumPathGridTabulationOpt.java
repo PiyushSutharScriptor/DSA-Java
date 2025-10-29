4a3ff16bcdf5d95bc3d2313171efc0ef68788cb0
Piyush <piyushsuthar6300@gmail.com>
2025-10-29 23:12:52 +0530

    Maximum sum path in grid tabulation opti

---
+++ DP/MaxSumPathGridTabulationOpt.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class MaxSumPathGridTabulationOpt{
+
+    public static int findMax(int[][] arr){
+
+        int m = arr.length;
+        int n = arr[0].length;
+
+        int[] dp = new int[n];
+        Arrays.fill(dp,-1);
+
+        for(int i=0 ; i<m ; i++){
+            int[] temp = new int[n];
+            for(int j=0 ; j<n ; j++){
+                if(i<0 || j<0){
+                    continue;
+                }
+
+                if(i==0 && j==0){
+                    temp[j]=arr[i][j];
+                    continue;
+                }
+
+                int up = (int)-1e8;
+                int left = (int)-1e8;
+
+                if(i>0) up = arr[i][j] + dp[j];
+                if(j>0) left = arr[i][j] + temp[j-1];
+
+                temp[j] = Math.max(up,left);
+            }
+            dp = temp;
+        }        
+
+        return dp[n-1];
+    }
+
+    public static void main(String[] args) {
+        int[][] grid = {{1,2,3},{4,5,6}};
+
+        int res = findMax(grid);
+        System.out.println(res);
+    }
+}
+
