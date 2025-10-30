70da79d637f6fa3a0e1bed7ad9226c1bf8932df6
Piyush <piyushsuthar6300@gmail.com>
2025-10-30 21:21:25 +0530

    Max sum in triangular grid recursion

---
+++ DP/MaxSumTriangleGridRecursion.java
@@ -0,0 +1,22 @@
+
+public class MaxSumTriangleGridRecursion {
+
+    public static int findPaths(int row, int col, int[][] arr){
+        int m = arr.length;
+        int n = arr[row].length;
+        if(row==m-1) return arr[row][col];
+        if(row>m-1 || col>n-1) return 0;
+
+        int down = arr[row][col] + findPaths(row+1, col, arr);
+        int diag = arr[row][col] + findPaths(row+1, col+1, arr);
+
+        return Math.max(down,diag);
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = {{1},{2,3},{3,6,7},{8,9,6,10}};
+
+        int res = findPaths(0,0,arr);
+        System.out.println(res);
+    }
+}
