9016dc074a8b480fffe27f0928a826551b0fbe4b
Piyush <piyushsuthar6300@gmail.com>
2025-11-01 23:43:01 +0530

    Update CherryPickUp2Memoization.java

--- DP/CherryPickUp2Memoization.java
+++ DP/CherryPickUp2Memoization.java
@@ -1,50 +1,49 @@
 import java.util.*;
 
-public class CherryPickUp2Memoization{
+public class CherryPickUp2Memoization {
 
-    public static int findMax(int i, int j1, int j2, int[][]arr,int[][][] dp){
+    public static int findMax(int i, int j1, int j2, int[][] arr, int[][][] dp) {
         int m = arr.length;
         int n = arr[0].length;
 
-        if(j1<0 || j2<0 || j1>=n || j2>=n) return (int)-1e8;
+        if (j1 < 0 || j2 < 0 || j1 >= n || j2 >= n) return (int) -1e8;
 
-        if(dp[i][j1][j2]!=Integer.MIN_VALUE) return dp[i][j1][j2];
-
-        if(i==m-1){
-            if(j1!=j2) return arr[i][j1]+arr[i][j2];
-            else return arr[i][j1];
+        if (i == m - 1) {
+            if (j1 == j2) return arr[i][j1];
+            else return arr[i][j1] + arr[i][j2];
         }
 
-        int maxi = (int)-1e8;
+        if (dp[i][j1][j2] != Integer.MIN_VALUE) return dp[i][j1][j2];
 
-        for(int dj1=-1 ; dj1<=1 ; dj1++){
-            for(int dj2=-1 ; dj2<=1 ; dj2++){
-                if(j1!=j2){
-                    maxi = Math.max(maxi, arr[i][j1]+arr[i][j2]+findMax(i+1, j1+dj1, j2+dj2,arr,dp));
-                }
-                else{
-                    maxi = Math.max(maxi,arr[i][j2]+findMax(i+1, j1+dj1, j2+dj2,arr,dp));
-                }
+        int maxi = (int) -1e8;
+
+        for (int dj1 = -1; dj1 <= 1; dj1++) {
+            for (int dj2 = -1; dj2 <= 1; dj2++) {
+                int value = 0;
+                if (j1 == j2) value = arr[i][j1];
+                else value = arr[i][j1] + arr[i][j2];
+
+                value += findMax(i + 1, j1 + dj1, j2 + dj2, arr, dp);
+                maxi = Math.max(maxi, value);
             }
         }
 
         return dp[i][j1][j2] = maxi;
     }
-    
+
     public static void main(String[] args) {
-        int[][] arr = {{2,3,1,2},{3,4,2,2},{5,6,3,5}};
+        int[][] arr = {{2, 3, 1, 2},{3, 4, 2, 2},{5, 6, 3, 5}};
 
         int m = arr.length;
         int n = arr[0].length;
 
         int[][][] dp = new int[m][n][n];
-        for(int[][] ar : dp){
-            for(int[] ar2 : ar){
-                Arrays.fill(ar2,Integer.MIN_VALUE);
+        for (int[][] ar : dp) {
+            for (int[] ar2 : ar) {
+                Arrays.fill(ar2, Integer.MIN_VALUE);
             }
         }
 
-        int res1 = findMax(0,0,n-1,arr,dp);
-        System.out.println(res1);
+        System.out.println(findMax(0, 0, n - 1, arr, dp));
     }
 }
