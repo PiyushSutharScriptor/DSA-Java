620935544043e124bb01eb7a94e28bf6d1a8a205
Piyush <piyushsuthar6300@gmail.com>
2025-11-04 22:55:11 +0530

    count subset with sum k tabulation

---
+++ DP/CountsSubsetsWithSumkTabulation.java
@@ -0,0 +1,37 @@
+public class CountsSubsetsWithSumkTabulation {
+
+    public static int findCount(int[] arr, int target){
+        int n = arr.length;
+
+        int dp[][] = new int[n][target+1];
+
+        for(int i=0 ; i<n ; i++){
+            dp[i][0] = 1;
+        }
+
+        if(arr[0]<=target) dp[0][arr[0]] = 1;
+
+        for(int idx=1; idx<n ; idx++){
+            for(int t=1 ; t<=target; t++){
+                int notTake = dp[idx-1][t];
+
+                int take = 0;
+                if(arr[idx]<=t){
+                    take = dp[idx-1][t-arr[idx]];
+                }
+
+                dp[idx][t] = take+notTake;
+            }
+        }
+
+        return dp[n-1][target];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,2,3};
+        int t = 6;
+
+        int res = findCount(arr,t);
+        System.out.println(res);
+    }
+}
