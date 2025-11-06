9ad122798c38f64a93a5f1df986b8f5d0172f33b
Piyush <piyushsuthar6300@gmail.com>
2025-11-07 00:21:58 +0530

    Minimum coins tabulation optimized

---
+++ DP/MinimumCoinsTabulationOpt.java
@@ -0,0 +1,39 @@
+public class MinimumCoinsTabulationOpt {
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 2, 4};
+        int amount = 3;
+        int n = arr.length;
+
+        int[] dp = new int[amount + 1];
+        int INF = (int)1e8;
+
+        dp[0] = 0;
+
+        for (int target = 1; target <= amount; target++) {
+            if (target % arr[0] == 0)
+                dp[target] = target / arr[0];
+            else
+                dp[target] = INF;
+        }
+
+        for (int i = 1; i < n; i++) {
+
+            int[] temp = new int[amount+1];
+            temp[0] = 0;
+            
+            for (int target = 1; target <= amount; target++) {
+                int notTake = dp[target];
+                int take = INF;
+                if (arr[i] <= target)
+                    take = 1 + dp[target - arr[i]];
+                temp[target] = Math.min(take, notTake);
+            }
+
+            dp = temp;
+        }
+
+        int ans = dp[amount];
+        System.out.println(ans >= INF ? -1 : ans);
+    }
+}
+
