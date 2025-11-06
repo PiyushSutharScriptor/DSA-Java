6f1e47ce429c7930d28192b3d119220fd80a030e
Piyush <piyushsuthar6300@gmail.com>
2025-11-06 23:49:13 +0530

    minimum coins memoization

---
+++ DP/MinimumCoinsMemiozation.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class MinimumCoinsMemiozation{
+
+    public static int findMin(int idx, int[] arr, int amount, int[][] dp){
+
+        if(amount==0) return 0;
+
+        if(idx<0) return (int)1e8;
+
+        if(dp[idx][amount]!=-1) return dp[idx][amount];
+
+        int notTake = 0 + findMin(idx-1, arr, amount,dp);
+        int take = (int)1e8;
+
+        if(arr[idx]<=amount){
+            take = 1 + findMin(idx, arr, amount-arr[idx], dp);
+        }
+
+        return dp[idx][amount] = Math.min(take,notTake);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,1,2,4};
+        int amount = 3;
+
+        int n = arr.length;
+
+        int[][] dp = new int[n][amount+1];
+        for(int ar[] : dp){
+            Arrays.fill(ar,-1);
+        }
+
+        int ans = findMin(n-1, arr,amount,dp);
+
+        if(ans==(int)1e8) System.out.println(-1);
+        else System.out.println(ans);
+    }
+}
