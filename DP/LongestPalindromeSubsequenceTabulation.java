68fd6f75cb1a644dca742c156989858f3837abf4
Piyush <piyushsuthar6300@gmail.com>
2025-11-09 21:25:39 +0530

    Longest palindrome subsequence Tabulation

---
+++ DP/LongestPalindromeSubsequenceTabulation.java
@@ -0,0 +1,25 @@
+
+public class LongestPalindromeSubsequenceTabulation{
+    public static void main(String[] args) {
+        String s1 = "bbbab";
+
+        String s2 = new StringBuilder(s1).reverse().toString();
+        int n = s1.length();
+
+        int[][] dp = new int[n+1][n+1];
+
+        for(int idx1=1 ; idx1<=n ; idx1++){
+            for(int idx2=1 ; idx2<=n ; idx2++){
+
+                if(s1.charAt(idx1-1)==s2.charAt(idx2-1)){
+                    dp[idx1][idx2] = 1 + dp[idx1-1][idx2-1];
+                }
+                else{
+                    dp[idx1][idx2] = Math.max(dp[idx1-1][idx2] , dp[idx1][idx2-1]);
+                }
+            }
+        }
+
+        System.out.println(dp[n][n]);
+    }
+}
