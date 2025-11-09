1fcdeaa76dcccc59bc78836a406aa3ebb0549b9b
Piyush <piyushsuthar6300@gmail.com>
2025-11-09 23:12:08 +0530

    Min insertion to make string palindrome recursion

---
+++ DP/MinInsertionToMakeStrPalindromeRecursion.java
@@ -0,0 +1,23 @@
+public class MinInsertionToMakeStrPalindromeRecursion {
+
+    public static int findMax(int idx1, int idx2, String s1, String s2){
+        if(idx1<0 || idx2<0) return 0;
+
+        if(s1.charAt(idx1)==s2.charAt(idx2)){
+            return 1+findMax(idx1-1, idx2-1, s1, s2);
+        }
+        else{
+            return Math.max(findMax(idx1-1, idx2, s1, s2) , findMax(idx1, idx2-1, s1, s2));
+        }
+    }
+
+    public static void main(String[] args) {
+        String s1 = "abcaa";
+
+        String s2 = new StringBuilder(s1).reverse().toString();
+        int n = s1.length();
+
+        int res = findMax(n-1, n-1, s1, s2);
+        System.out.println(n-res);
+    }
+}
