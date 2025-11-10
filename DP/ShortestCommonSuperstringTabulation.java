12e04a156936282aaf4294b014305053ac682637
Piyush <piyushsuthar6300@gmail.com>
2025-11-10 15:41:36 +0530

    Shortest common superstring tabulation

---
+++ DP/ShortestCommonSuperstringTabulation.java
@@ -0,0 +1,35 @@
+public class ShortestCommonSuperstringTabulation {
+
+    public static int findCount(String s1, String s2){
+
+        int m = s1.length();
+        int n = s2.length();
+
+        int[][] dp = new int[m+1][n+1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        
+        return dp[m][n];
+
+    }
+    public static void main(String[] args) {
+        String s1 = "mno";
+        String s2 = "nop";
+
+        int m = s1.length();
+        int n = s2.length();
+
+        int res = findCount(s1,s2);
+        int ans = (m+n)-res;
+        System.out.println(ans);
+    }
+}
+
