b0542a136a3cf82e0746ffbb41dc4789c7107714
Piyush <piyushsuthar6300@gmail.com>
2025-11-10 15:41:22 +0530

    Shortest common superstring recursion

---
+++ DP/ShortestCommonSuperstringRecursion.java
@@ -0,0 +1,25 @@
+public class ShortestCommonSuperstringRecursion {
+
+    public static int findCount(int idx1, int idx2, String s1, String s2){
+
+        if(idx1<0 || idx2<0) return 0;
+
+        if(s1.charAt(idx1)==s2.charAt(idx2)){
+            return 1 + findCount(idx1-1, idx2-1, s1, s2);
+        }
+        
+        return 0 + Math.max(findCount(idx1-1, idx2, s1, s2) , findCount(idx1, idx2-1, s1, s2));
+
+    }
+    public static void main(String[] args) {
+        String s1 = "dynamic";
+        String s2 = "program";
+
+        int m = s1.length();
+        int n = s2.length();
+
+        int res = findCount(m-1,n-1,s1,s2);
+        int ans = (m+n)-res;
+        System.out.println(ans);
+    }
+}
