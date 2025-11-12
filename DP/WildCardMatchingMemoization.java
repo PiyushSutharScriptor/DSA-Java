9107013c281683ccf0d9097cc16efc4fb5d082d7
Piyush <piyushsuthar6300@gmail.com>
2025-11-12 19:54:51 +0530

    Wild card matching memoization

---
+++ DP/WildCardMatchingMemoization.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class WildCardMatchingMemoization {
+
+    public static boolean findMatch(int idx1, int idx2, String s1, String s2, int[][] dp){
+
+        if(idx1<0 && idx2<0) return true;
+        if(idx1<0 && idx2>=0) return false;
+        if(idx1>=0 && idx2<0){
+            for(int i=0 ; i<=idx1 ; i++){
+                if(s1.charAt(i)!='*') return false;
+            }
+            return true;
+        }
+
+        if(dp[idx1][idx2]!=-1) return dp[idx1][idx2]==1 ? true : false;
+
+        if(s1.charAt(idx1)==s2.charAt(idx2) || s1.charAt(idx1)=='?'){
+            boolean ans1 = findMatch(idx1-1, idx2-1, s1, s2, dp);
+            dp[idx1][idx2] = ans1 ? 1 : 0;
+            return ans1;
+        }
+        else{
+            if(s1.charAt(idx1)=='*'){
+                boolean ans2 = findMatch(idx1-1, idx2, s1, s2, dp) || findMatch(idx1, idx2-1, s1, s2,dp);
+                dp[idx1][idx2] = ans2 ? 1 : 0;
+                return ans2;    
+            }
+        }
+
+        return false;
+        
+    }
+    public static void main(String[] args) {
+        String s1 = "x?y*z";
+        String s2 = "xaylmz";
+
+        int m = s1.length();
+        int n = s2.length();
+
+        int[][] dp = new int[m][n];
+        for(int[] ar : dp){
+            Arrays.fill(ar,-1);
+        }
+
+        System.out.println(findMatch(m-1, n-1, s1, s2, dp));
+    }
+}
