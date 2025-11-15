c256271a9422c0e86303577bee14360a7ff4afa8
Piyush <piyushsuthar6300@gmail.com>
2025-11-15 22:56:09 +0530

    Longest increasing subsequence Tabulation

---
+++ DP/LongestIncreasingSubsequenceTabulation.java
@@ -0,0 +1,33 @@
+
+public class LongestIncreasingSubsequenceTabulation {
+
+    public static int findMax(int[] arr){
+
+        int n = arr.length;
+
+        int[][] dp = new int[n+1][n+1];
+
+        for(int curr=n-1 ; curr>=0 ; curr--){
+            for(int prev=curr-1 ; prev>=-1 ; prev--){
+
+                int notTake = dp[curr+1][prev+1];
+
+                int take = 0;
+                if(prev==-1 || arr[curr]>arr[prev]){
+                    take = 1 + dp[curr+1][curr+1];
+                }
+
+                dp[curr][prev+1] = Math.max(notTake, take);
+            }
+        }
+        
+        return dp[0][0];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10,9,2,5,3,7,101,18};
+
+        int res = findMax(arr);
+        System.out.println(res);
+    }
+}
