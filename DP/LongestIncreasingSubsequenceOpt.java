d00b8981e2218e85dac667f2d2ba590a30b83d35
Piyush <piyushsuthar6300@gmail.com>
2025-11-16 23:05:25 +0530

    Longest Increasing Subsequence Optimized

---
+++ DP/LongestIncreasingSubsequenceOpt.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class LongestIncreasingSubsequenceOpt {
+    public static void main(String[] args) {
+        int[] arr = {10, 22, 9, 33, 21, 50, 41, 60};
+        int n = arr.length;
+
+        int[] dp = new int[n];
+        Arrays.fill(dp,1);
+
+        for(int i=1 ; i<n ; i++){
+            for(int j=0 ; j<i ; j++){
+                if(arr[i]>arr[j] && dp[j]+1>dp[i]){
+                    dp[i] = dp[j]+1;
+                }
+            }
+        }
+
+        int maxi = 0;
+        for(int el : dp){
+            maxi = Math.max(maxi,el);
+        }
+
+        System.out.println(maxi);
+    }
+}
