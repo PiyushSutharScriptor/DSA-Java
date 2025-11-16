ef65996e3c2f9f0c94eccd67ee3330d22024dc40
Piyush <piyushsuthar6300@gmail.com>
2025-11-16 23:27:15 +0530

    Update LongestDivisibleSubsetOptimized.java

--- DP/LongestDivisibleSubsetOptimized.java
+++ DP/LongestDivisibleSubsetOptimized.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class LongestDivisibleSubsetOptimized {
     public static List<Integer> findLDS(int[] arr) {
