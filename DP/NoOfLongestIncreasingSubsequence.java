9fa4f78140d9e7e33439fcb050767dc0d27da783
Piyush <piyushsuthar6300@gmail.com>
2025-11-18 17:26:52 +0530

    Update NoOfLongestIncreasingSubsequence.java

--- DP/NoOfLongestIncreasingSubsequence.java
+++ DP/NoOfLongestIncreasingSubsequence.java
@@ -29,7 +29,7 @@ public class NoOfLongestIncreasingSubsequence {
         for(int el : dp){
             maxi = Math.max(maxi, el);
         }
-
+           
         int res = 0;
 
         for(int i=0 ; i<n ; i++){
