e3d64d0a5c403fcf0f1f0aa50e72897af485f9c7
Piyush <piyushsuthar6300@gmail.com>
2025-11-20 21:40:02 +0530

    Matrix chain multiplication using tabulation

---
+++ DP/MatrixChainMultiplicationTabulation.java
@@ -0,0 +1,40 @@
+
+import java.util.*;
+
+public class MatrixChainMultiplicationTabulation{
+
+    public static int findMin(int[] arr){
+        int n = arr.length;
+
+        int[][] dp = new int[n][n];
+        
+        for(int i=n-1 ; i>=1 ; i--){
+
+            for(int j=i+1 ; j<n ; j++){
+                
+                int mini = Integer.MAX_VALUE;
+
+                for(int k=i ; k<j ; k++){
+                    int steps = arr[i-1]*arr[k]*arr[j]
+                                + dp[i][k]
+                                + dp[k+1][j];
+                    
+                    mini = Math.min(mini,steps);
+                }
+
+                dp[i][j] = mini;
+            }
+
+        }
+
+        return dp[1][n-1];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 15, 20, 25};
+
+        int res = findMin(arr);
+        System.out.println(res);
+    }
+}
+
