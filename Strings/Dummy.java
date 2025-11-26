775f630a70e4ceffa651a3e91f8f0c0fc0ad7dce
Piyush <piyushsuthar6300@gmail.com>
2025-11-26 17:26:24 +0530

    Testing code

---
+++ Strings/Dummy.java
@@ -0,0 +1,10 @@
+public class Dummy {
+    public static void main(String[] args) {
+        String[] a = {"ab","abc"};
+        String[] b = {"ab","abc"};
+
+        if(a[0]==b[0]) System.out.println("yes");
+        else System.out.println("no");
+    }
+}
+ 
