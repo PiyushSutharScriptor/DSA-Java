c7dcdab378bd7141f9a8ab6b8662888b82a84ae3
Piyush <piyushsuthar6300@gmail.com>
2025-12-05 18:19:01 +0530

    Three sum using list

---
+++ DSA Patterns/TwoPointers/ThreeSumUsingList.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class ThreeSumUsingList {
+    public static void main(String[] args) {
+        int[] nums = {0,0,0,0};
+        int n = nums.length;
+
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+
+        for(int i=0 ; i<n-2 ; i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+
+            int l = i+1;
+            int r = n-1;
+
+            while(l<r){
+                int sum = nums[i]+nums[l]+nums[r];
+
+                if(sum==0){
+                    res.add(new ArrayList<>(Arrays.asList(nums[i] , nums[l] , nums[r])));
+
+                    while(l<r){
+                        if(nums[l]==nums[l-1]) l++;
+                    }
+                    while(l<r){
+                        if(nums[r]==nums[r+1]) r--;
+                    }
+                }
+                else if(sum<0){
+                    l++;
+                }
+                else{
+                    r--;
+                }
+            }
+        }
+
+        System.out.println(res);
+
+    }    
+}
