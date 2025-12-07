86bd11ef9206b064ca8750af1e753b5d2328f601
Piyush <piyushsuthar6300@gmail.com>
2025-12-07 12:40:22 +0530

    Sort colors updated

--- DSA Patterns/TwoPointers/SortColorsDutchFlagAlgo.java
+++ DSA Patterns/TwoPointers/SortColorsDutchFlagAlgo.java
@@ -1,5 +1,36 @@
 public class SortColorsDutchFlagAlgo {
     public static void main(String[] args) {
-        
+        int[] arr = {2,1,0,2,1,0,1,2,0,1,2,0};
+
+        int n = arr.length;
+
+        int low = 0;
+        int mid = 0;
+        int high = n-1;
+
+        while(mid<=high){
+            if(arr[mid]==0){
+                int temp = arr[mid];
+                arr[mid] = arr[low];
+                arr[low] = temp;
+
+                low++;
+                mid++;
+            }
+            else if(arr[mid]==2){
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+
+                high--;
+            }
+            else{
+                mid++;
+            }
+        }
+
+        for(int el : arr){
+            System.out.print(el + " ");
+        }
     }
 }
