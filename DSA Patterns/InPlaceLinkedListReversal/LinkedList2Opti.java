36b7db5f5ba0bf3551ec6fce08e1d895aaa8d826
Piyush <piyushsuthar6300@gmail.com>
2025-12-14 23:12:22 +0530

    Linked list 2 leetcode optimized

---
+++ DSA Patterns/InPlaceLinkedListReversal/LinkedList2Opti.java
@@ -0,0 +1,81 @@
+class Node{
+    Node next;
+    int data;
+
+    Node(int data1, Node node1){
+        this.next = node1;
+        this.data = data1;
+    }
+
+    Node(int data2){
+        this.data = data2;
+        this.next = null;
+    }
+}
+
+public class LinkedList2Opti {
+
+    public static void print(Node root){
+        while(root!=null){
+            System.out.print(root.data + " ");
+            root = root.next;
+        }
+        System.out.println();
+    }
+
+    public static Node reverse(Node root){
+        Node curr = root;
+        Node prev = null;
+
+        while(curr!=null){
+            Node nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        return prev;
+    }
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        Node n2 = new Node(2);
+        Node n3 = new Node(3);
+        Node n4 = new Node(4);
+        Node n5 = new Node(5);
+        Node n6 = new Node(6);
+
+        head.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+        n5.next = n6;
+
+        int left = 2;
+        int right = 5;
+
+        Node temp = new Node(0);
+        temp.next = head;
+
+        Node leftNode = temp;
+        for(int i=1 ; i<left ; i++){
+            leftNode = leftNode.next;
+        }
+
+        Node rightNode = leftNode;
+        for(int i=left ; i<=right ; i++){
+            rightNode = rightNode.next;
+        }
+
+        Node nextLeft = leftNode.next;
+        Node rightConnect = rightNode.next;
+        rightNode.next = null;
+
+        Node newHead = reverse(nextLeft);
+
+        leftNode.next = newHead;
+        nextLeft.next = rightConnect;
+
+        print(temp.next);
+    }
+}
+
