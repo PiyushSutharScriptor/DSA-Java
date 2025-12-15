8693d612c40feeacf5c092592ec62bdfa5085e77
Piyush <piyushsuthar6300@gmail.com>
2025-12-15 20:39:36 +0530

    Tree creation

---
+++ DSA Patterns/TreeBFS/TreeCreation.java
@@ -0,0 +1,21 @@
+class Node{
+    Node left;
+    Node right;
+    int data;
+
+    Node(int data1){
+        this.data = data1;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class TreeCreation{
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+    }
+}
