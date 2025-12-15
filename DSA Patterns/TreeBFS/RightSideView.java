3e35376b69d23d6fd09bdfc32a553370cde1464f
Piyush <piyushsuthar6300@gmail.com>
2025-12-15 23:15:21 +0530

    Binary tree right side view

---
+++ DSA Patterns/TreeBFS/RightSideView.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+class Node{
+    Node left;
+    Node right;
+    int data;
+
+    Node(int data1){
+        this.data = data1;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class RightSideView {
+
+    public static List<Integer> rightView(Node root){
+        if(root==null) return new ArrayList<>();
+
+        List<Integer> res = new ArrayList<>();
+        Queue<Node> que = new LinkedList<>();
+        que.add(root);
+        
+        while(!que.isEmpty()){
+            int size = que.size();
+
+            for(int i=0 ; i<size ; i++){
+                Node el = que.poll();
+
+                if(el.left!=null){
+                    que.add(el.left);
+                }
+                if(el.right!=null){
+                    que.add(el.right);
+                }
+
+                if(i==size-1) res.add(el.data);
+            }
+        }
+
+        return res;
+
+    }
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        List<Integer> res = rightView(root);
+        System.out.println(res);
+    }   
+}
