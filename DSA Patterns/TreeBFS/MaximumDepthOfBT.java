1a4fde3feb9538555cdf6ba96df51ba32ee105a3
Piyush <piyushsuthar6300@gmail.com>
2025-12-15 22:35:23 +0530

    Maximum depth of binary tree

---
+++ DSA Patterns/TreeBFS/MaximumDepthOfBT.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+class Node{
+    Node left;
+    Node right;
+    int data;
+
+    Node(int data1){
+        this.data = data1;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class MaximumDepthOfBT {
+
+    public static int findMax(Node root){
+        if(root==null) return 0;
+
+        Queue<Node> que = new LinkedList<>();
+        que.add(root);
+        int count = 0;
+
+        while(!que.isEmpty()){
+            int size = que.size();
+            count++;
+
+            for(int i=0 ; i<size ; i++){
+                Node el = que.poll();
+
+                if(el.left!=null){
+                    que.add(el.left);
+                }
+
+                if(el.right!=null){
+                    que.add(el.right);
+                }
+            }   
+        }
+
+        return count;
+
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        int res = findMax(root);
+        System.out.println(res);
+    }
+}
