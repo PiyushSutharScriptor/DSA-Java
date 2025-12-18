829e714a2403b908fa332208c9b6056eaffa9684
Piyush <piyushsuthar6300@gmail.com>
2025-12-18 08:53:45 +0530

    Path sum 1 leetcode

---
+++ DSA Patterns/TreeDFS/PathSum.java
@@ -0,0 +1,37 @@
+class Node{
+    Node left;
+    Node right;
+    int data;
+
+    Node(int data1){
+        this.data = data1;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class PathSum{
+
+    public static boolean findPath(Node root, int target){
+        if(root==null) return false;
+
+        if(root.left==null && root.right==null){
+            return (target==root.data);
+        }
+
+        return 
+        findPath(root.left, target-root.data) ||
+        findPath(root.right, target-root.data);
+
+    }
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        
+        int target = 7;
+        System.out.println(findPath(root,target));
+    }
+}
