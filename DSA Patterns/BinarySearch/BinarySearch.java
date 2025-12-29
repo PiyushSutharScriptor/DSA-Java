6d822cd7ccd3f5493039a248f37ac19b87b1552e
Piyush <piyushsuthar6300@gmail.com>
2025-12-29 23:30:10 +0530

    Binary Search overflow case

---
+++ DSA Patterns/BinarySearch/BinarySearch.java
@@ -0,0 +1,25 @@
+public class BinarySearch{
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int target = 4;
+
+        int n = arr.length;
+
+        int l = 0;
+        int r = n-1;
+        int res = -1;
+
+        while(l<=r){
+            int mid = (l+r)/2;
+
+            if(arr[mid]==target){
+                res = mid;
+                break;
+            }
+            else if(arr[mid]>target) r=mid-1;
+            else l = mid+1;
+        }
+
+        System.out.println(res);
+    }
+}
