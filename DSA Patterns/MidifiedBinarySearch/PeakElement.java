110b89cdd085d03cc361b4cc2f5c30c4d1638970
Piyush <piyushsuthar6300@gmail.com>
2026-01-04 22:14:33 +0530

    Optimization for peak element for leetcode

--- DSA Patterns/MidifiedBinarySearch/PeakElement.java
+++ DSA Patterns/MidifiedBinarySearch/PeakElement.java
@@ -2,6 +2,11 @@ public class PeakElement{
 
     public static int findEle(int[] arr){
         int n = arr.length;
+
+        if(n==1) return 0;
+        if(arr[0]>arr[1]) return 0;
+        if(arr[n-1]>arr[n-2])return n-1;
+
         int low = 1;
         int high = n-1;
 
@@ -11,16 +16,15 @@ public class PeakElement{
             if(arr[mid]>arr[low] && arr[mid]>arr[high]){
                 return mid;
             }
-            else if(arr[low-1]<arr[low]){
-                low = mid+1;
+            else if(arr[mid]>arr[mid+1] && arr[mid]<arr[mid-1]){
+                high = mid-1;
             }
             else{
-                high = mid-1;
+                low = mid+1;
             }
         }
 
         return -1;
-
     }
 
     public static void main(String[] args) {
