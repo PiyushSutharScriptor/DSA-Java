f57e1fa44feeac7593ab9bd73f0a97d7c7958a33
Piyush <piyushsuthar6300@gmail.com>
2026-01-07 00:12:29 +0530

    kth largest element better approach

---
+++ DSA Patterns/Heap/FindKthLargestElementBetter.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class FindKthLargestElementBetter {
+
+    public static int findEle(int[] arr, int k){
+        PriorityQueue<Integer> max = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int el : arr){
+            max.add(el);
+        }
+
+        for(int i=0 ; i<k-1 ; i++){
+            max.poll();
+        }
+
+        return max.peek();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3,4};
+        int k = 2;
+
+        int res = findEle(arr,k);
+        System.out.println(res);
+    }
+}
