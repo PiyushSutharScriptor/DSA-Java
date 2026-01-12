61396fa7ea3d32ec39466cb7a7bed77b0560d1b3
Piyush <piyushsuthar6300@gmail.com>
2026-01-12 22:48:00 +0530

    Merge k sorted list brute

---
+++ DSA Patterns/KWayMerge/mergeKSortedListBrute.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+class Node{
+    Node next;
+    int data;
+
+    Node(int data1){
+        this.data = data1;
+        this.next = null;
+    }
+}
+
+public class mergeKSortedListBrute {
+
+    public static void print(Node root){
+        while(root!=null){
+            System.out.print(root.data + " ");
+            root = root.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        
+        //create list
+        Node root = new Node(1);
+        root.next = new Node(4);
+        root.next.next = new Node(5);
+
+        Node root2 = new Node(1);
+        root2.next = new Node(3);
+        root2.next.next = new Node(4);
+
+        Node root3 = new Node(2);
+        root3.next = new Node(6);
+
+        print(root);
+        print(root2);
+        print(root3);
+
+        Node[] arr = {root,root2,root3};
+
+        PriorityQueue<Node> heap = new PriorityQueue<>((a,b) -> a.data-b.data);
+
+        for(Node head : arr){
+            Node thead = head;
+            while(thead!=null){
+                heap.add(thead);
+                thead = thead.next;
+            }
+        }
+
+        //create a ll
+        Node res = new Node(-1);
+        Node mover = res;
+        while(!heap.isEmpty()){
+            Node temp = heap.poll();
+            mover.next = new Node(temp.data);
+            mover = mover.next;
+        }
+
+        print(res.next);
+
+    }
+}
