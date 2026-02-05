a8ee25ea591c0ead09bc43c08688e3a38abad8b0
Piyush <piyushsuthar6300@gmail.com>
2026-02-05 22:58:57 +0530

    Longest Palindromic Subsequence Memoization

---
+++ DSA Patterns/DP/LongestPalindromicSubsequenceMemoization.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class LongestPalindromicSubsequenceMemoization {
+
+    public static int findMax(int idx1, int idx2, String s1, String s2, int[][] dp){
+
+        if(idx1<0 || idx2<0) return 0;
+
+        if(dp[idx1][idx2]!=-1) return dp[idx1][idx2];
+
+        if(s1.charAt(idx1)==s2.charAt(idx2)){
+            return dp[idx1][idx2] = 1+findMax(idx1-1, idx2-1, s1, s2, dp);
+        }
+
+        return dp[idx1][idx2] = Math.max(findMax(idx1-1, idx2, s1, s2, dp),findMax(idx1, idx2-1, s1, s2, dp));
+
+    }
+    public static void main(String[] args) {
+        String str = "bbbab";
+
+        String str2 = new StringBuilder(str).reverse().toString();
+
+        int n = str.length();
+
+        int[][] dp = new int[n][n];
+        for(int[] ar : dp){
+            Arrays.fill(ar,-1);
+        }
+
+        int res = findMax(n-1,n-1,str,str2,dp);
+        System.out.println(res);
+    }    
+}
+
