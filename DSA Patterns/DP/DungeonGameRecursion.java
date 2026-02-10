29bd7a32ced98a72dc60f1fb17a9045bee31ad85
Piyush <piyushsuthar6300@gmail.com>
2026-02-10 23:51:25 +0530

    DungeonGame Recursion

---
+++ DSA Patterns/DP/DungeonGameRecursion.java
@@ -0,0 +1,29 @@
+public class DungeonGameRecursion {
+
+    public static int maxi = (int)1e8;
+
+    public static int findMin(int x, int y, int[][] arr){
+        int m = arr.length;
+        int n = arr[0].length;
+
+        if(x == m-1 && y == n-1){
+            return Math.max(1, 1 - arr[x][y]);
+        }
+
+        if(x >= m || y >= n) return maxi;
+
+        int down = findMin(x+1, y, arr);
+        int right = findMin(x, y+1, arr);
+
+        int need = Math.min(down, right) - arr[x][y];
+
+        return Math.max(1, need);
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = {{-2,-3,3},{-5,-10,1},{10,30,-5}};
+
+        int res = findMin(0, 0, arr);
+        System.out.println(res); 
+    }
+}
