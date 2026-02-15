1aedf3bec497a36d88b5aa8308423bfeaa580a29
Piyush <piyushsuthar6300@gmail.com>
2026-02-15 20:36:09 +0530

    Clone Graph brute

---
+++ DSA Patterns/Graph/CloneGraph.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class CloneGraphBrute {
+    public Node cloneGraph(Node node) {
+        if(node == null) return null;
+
+        Node copy = new Node(node.val);
+
+        for(Node nei : node.neighbors){
+            copy.neighbors.add(cloneGraph(nei));
+        }
+
+        return copy;
+    }
+}
